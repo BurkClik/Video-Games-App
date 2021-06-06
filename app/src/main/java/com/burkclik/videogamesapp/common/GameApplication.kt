97cd77ccf5a49b1c999c9362e00c4adfbe08ebc1
BurkClik <burk.clik@gmail.com>
2021-06-06 20:36:44 +0300

    Add Hilt. Create Hilt Application. Create Network Module

---
+++ app/src/main/java/com/burkclik/videogamesapp/common/GameApplication.kt
@@ -0,0 +1,7 @@
+package com.burkclik.videogamesapp.common
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GameApplication : Application()
---
+++ app/src/main/java/com/burkclik/videogamesapp/common/NetworkModule.kt
@@ -0,0 +1,42 @@
+package com.burkclik.videogamesapp.common
+
+import com.burkclik.videogamesapp.data.remote.api.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    @Provides
+    fun provideApi(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Provides
+    fun provideHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addNetworkInterceptor(loggingInterceptor)
+            .build()
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+}
