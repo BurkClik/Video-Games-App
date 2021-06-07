119295a1d21952ed1dc00b90c39e1d53577706e5
BurkClik <burk.clik@gmail.com>
2021-06-08 00:55:22 +0300

    Delete unnecessary import

--- app/src/main/java/com/burkclik/videogamesapp/ui/detail/GameDetailFragment.kt
+++ app/src/main/java/com/burkclik/videogamesapp/ui/detail/GameDetailFragment.kt
@@ -1,12 +1,11 @@
 package com.burkclik.videogamesapp.ui.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.burkclik.videogamesapp.R
 import com.burkclik.videogamesapp.databinding.FragmentGameDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +20,7 @@ class GameDetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentGameDetailBinding.inflate(inflater, container, false)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
