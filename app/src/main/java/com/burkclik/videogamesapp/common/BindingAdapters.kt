e700b336b11915982c241135a99818ff443ad37f
BurkClik <burk.clik@gmail.com>
2021-06-08 00:42:45 +0300

    Create BindableItem.kt for GenericAdapter.kt

--- app/src/main/java/com/burkclik/videogamesapp/common/BindingAdapters.kt
+++ app/src/main/java/com/burkclik/videogamesapp/common/BindingAdapters.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.burkclik.videogamesapp.R
 
 @BindingAdapter("app:imageUrl")
 fun ImageView.imageUrl(imageUrl: String?) {
@@ -18,4 +19,13 @@ fun ImageView.imageUrl(imageUrl: String?) {
 @BindingAdapter("app:setVisibility")
 fun setVisibility(view: View, demoText: String) {
     view.isVisible = demoText.length < 3
+}
+
+@BindingAdapter("app:favoriteIcon")
+fun setFavoriteIcon(image: ImageView, state: Boolean) {
+    if (!state) {
+        image.setImageResource(R.drawable.ic_favorite)
+    } else {
+        image.setImageResource(R.drawable.ic_favorite_true)
+    }
 }
