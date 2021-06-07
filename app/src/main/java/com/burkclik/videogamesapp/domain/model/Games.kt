b6fae51a7deb9eb74e68f83d864d8d8a84ba8608
BurkClik <burk.clik@gmail.com>
2021-06-08 00:59:38 +0300

    Make BindableItem

--- app/src/main/java/com/burkclik/videogamesapp/domain/model/Games.kt
+++ app/src/main/java/com/burkclik/videogamesapp/domain/model/Games.kt
@@ -1,9 +1,11 @@
 package com.burkclik.videogamesapp.domain.model
 
+import com.burkclik.videogamesapp.common.BindableItem
+
 data class Games(
     val id: Int,
     val name: String,
     val released: String,
     val backgroundImage: String,
     val rating: String,
-)
+) : BindableItem
