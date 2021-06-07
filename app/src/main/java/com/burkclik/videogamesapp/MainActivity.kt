f8cb3a21503edefbb3083f53440a457084343e69
BurkClik <burk.clik@gmail.com>
2021-06-08 00:44:19 +0300

    Create bottom_navigation_bar.xml and add to activity_main.xml

--- app/src/main/java/com/burkclik/videogamesapp/MainActivity.kt
+++ app/src/main/java/com/burkclik/videogamesapp/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.burkclik.videogamesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.burkclik.videogamesapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
