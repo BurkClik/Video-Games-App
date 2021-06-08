774db63a1bc71f0e2377464ce08e8d4413006e49
BurkClik <burk.clik@gmail.com>
2021-06-08 17:20:10 +0300

    Delete unnecessary parameters.

--- app/src/main/java/com/burkclik/videogamesapp/common/Resource.kt
+++ app/src/main/java/com/burkclik/videogamesapp/common/Resource.kt
@@ -2,13 +2,7 @@ package com.burkclik.videogamesapp.common
 
 sealed class Resource<out T> {
     class Success<T>(val data: T) : Resource<T>()
-    class Error(
-        val exception: Throwable? = null,
-        val message: String? = null,
-        val statusCode: Int? = null
-    ) :
-        Resource<Nothing>()
-
+    class Error(val exception: Throwable? = null) : Resource<Nothing>()
     object Loading : Resource<Nothing>()
 }
 
